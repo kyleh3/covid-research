be1bb3ea0d5b2b7f8527496dddc7dce166b66113
Kyle Heatherly <143746645+kyleh3@users.noreply.github.com>
2024-06-20 16:02:11 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,33 +1,46 @@
-import dash
-from dash import html, dcc
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input   # pip install dash
+import pandas as pd                                                     # pip install pandas
 import plotly.express as px
-import pandas as pd
+import dash_mantine_components as dmc                                   # pip install dash-mantine-components==0.12.1
 
-# Import data from GitHub
-df = pd.read_csv(r'./country-altmetric.csv')
+# Incorporate data
+df = pd.read_csv(r'./org-allmetrics.csv')
 print(df)
 
-# Initialize the Dash app
-app = dash.Dash(__name__)
+# Initialize the app - incorporate a Dash Mantine theme
+external_stylesheets = [dmc.theme.DEFAULT_COLORS]
+app = Dash(external_stylesheets=external_stylesheets)
 
-# Define app layout
-app.layout = html.Div([
-    html.H1("COVID-19 Research Impact by Country"),
-    dcc.Graph(
-        id='choropleth-map',
-        figure=px.choropleth(
-            data_frame=df,
-            locations='country',  # This should match the column name in your DataFrame
-            locationmode='country names',  # Set the location mode based on your data
-            color='altmetric',  # This sets the color value based on your data
-            hover_name='country',  # Hover information based on your data
-            title='Global Reach of COVID-19 Research',
-            height=700,
-            width=1200
-        )
-    )
-])
+# App layout
+app.layout = dmc.Container([
+    dmc.Title('Top Research Organizations for COVID-19: Publications, Citations, and Altmetric', color="blue", size="h3"),
+    dmc.RadioGroup(
+            [dmc.Radio(i, value=i) for i in ['Publications', 'Citations', 'Altmetric']],
+            id='my-dmc-radio-item',
+            value='Publications',
+            size="sm"
+        ),
+    dmc.Grid([
+        dmc.Col([
+            dash_table.DataTable(data=df.to_dict('records'), page_size=12, style_table={'overflow': 'auto'})
+        ], span=6),
+        dmc.Col([
+            dcc.Graph(figure={}, id='graph-placeholder')
+        ], span=6),
+    ]),
 
-# Run the app
+], fluid=True)
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='graph-placeholder', component_property='figure'),
+    Input(component_id='my-dmc-radio-item', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='Country', y=col_chosen, histfunc='sum')
+    return fig
+
+# Run the App
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0')
